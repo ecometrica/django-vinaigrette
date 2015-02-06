e1618a305110922fbb30e402c9e62fb6fcd1dcd9
Eduardo S. Klein <eklein@123imprim.com>
2015-02-06 17:41:15 +0100

    collecting full property string to include in .po

--- vinaigrette/__init__.py
+++ vinaigrette/__init__.py
@@ -12,20 +12,20 @@ VERSION = "0.4.0"
 
 class VinaigretteError(Exception):
     pass
-    
+
 _registry = {}
 
 DOUBLE_PERCENTAGE_RE = re.compile(u'%(?!\()')
 
 def _vinaigrette_pre_save(sender, instance, **kwargs):
     setattr(instance, '_vinaigrette_saving', True)
-    
+
 def _vinaigrette_post_save(sender, instance, **kwargs):
     delattr(instance, '_vinaigrette_saving')
 
-def register(model, fields, restrict_to=None, manager=None):
+def register(model, fields, restrict_to=None, manager=None, properties=None):
     """Tell vinaigrette which fields on a Django model should be translated.
-    
+
     Arguments:
     model -- The relevant model class
     fields -- A list or tuple of field names. e.g. ['name', 'nickname']
@@ -33,27 +33,30 @@ def register(model, fields, restrict_to=None, manager=None):
         of objects to collect translation strings from.
     manager -- Optional. A reference to a manager -- e.g. Person.objects -- to use
         when collecting translation strings.
-        
+    properties -- A dictionary of "read only" properties that are composed by more that one field e.g. {'full_name': ['first_name', 'last_name']}
+
     Note that both restrict_to and manager are only used when collecting translation
     strings. Gettext lookups will always be performed on relevant fields for all
     objects on registered models.
     """
-    
+
     global _registry
     _registry[model] = {
         'fields': fields,
         'restrict_to': restrict_to,
-        'manager': manager
+        'manager': manager,
+        'properties': properties,
     }
+
     for field in fields:
         setattr(model, field, VinaigretteDescriptor(field))
     model.untranslated = lambda self, fieldname: self.__dict__[fieldname]
-    
+
     pre_save.connect(_vinaigrette_pre_save, sender=model)
     post_save.connect(_vinaigrette_post_save, sender=model)
 
 class VinaigretteDescriptor(object):
-    
+
     def __init__(self, name):
         self.name = name
 
