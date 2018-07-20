8b9c7dabf977e86bf4abaa544bdfe225d13ccd1b
Padraic Harley <padraic.harley@ecometrica.com>
2018-07-20 12:01:49 +0100

    Update structure from review
    Moves the test directory inside the app, which is the usual django app
    location for them.
    
    Removes settings from conftest, and conftest itself, and puts the
    settings.py and urls.py to a test_project directory.
    
    Adds pytest-django config to a pytest.ini to point at the
    test settings file

---
+++ test_project/settings.py
@@ -0,0 +1,42 @@
+SECRET_KEY = "thisisasecretkeyfortests.itisverysecure"
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+
+    'vinaigrette',
+)
+
+USE_I18N = True
+LANGUAGES = (
+    ('en', u'English'),
+    ('fr', u'Français'),
+)
+
+LANGUAGE_CODE = 'en'
+ROOT_URLCONF = 'test_project.urls'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+}
+
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+]
+
+SITE_ID = 1
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+    },
+]
