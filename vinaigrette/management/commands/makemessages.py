f11bbfccabe8140f91d9a2bbbebb1a51bf45751b
Jordi Gutiérrez Hermoso <jordi@ecometrica.com>
2020-01-10 12:50:44 -0500

    wip

--- vinaigrette/management/commands/makemessages.py
+++ vinaigrette/management/commands/makemessages.py
@@ -8,9 +8,10 @@ import re
 
 import vinaigrette
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import CommandError
 from django.core.management.commands import makemessages as django_makemessages
-from django.utils.translation import ugettext
+from django.utils.translation import gettext
+
 
 def _get_po_paths(locales=[]):
     """Returns paths to all relevant po files in the current project."""
@@ -23,7 +24,8 @@ def _get_po_paths(locales=[]):
     basedirs = set(map(os.path.abspath, filter(os.path.isdir, basedirs)))
 
     if not basedirs:
-        raise CommandError("This script should be run from the Django SVN tree or your project or app tree, or with the settings module specified.")
+        raise CommandError("This script should be run from the Django SVN tree or your project "
+                           "or app tree, or with the settings module specified.")
 
     if not locales:
         looks_like_locale = re.compile(r'[a-z]{2}')
@@ -43,21 +45,30 @@ def _get_po_paths(locales=[]):
                         po_paths.append(os.path.join(dirpath, f))
     return po_paths
 
+
 class Command(django_makemessages.Command):
-    help = ("Runs over the entire source tree of the current directory and "
+    help = (
+        "Runs over the entire source tree of the current directory and "
         "pulls out all strings marked for translation. It creates (or "
         "updates) a message file in the conf/locale (in the django tree) or "
         "locale (for project and application) directory. Also includes "
-        "strings from database fields handled by vinaigrette.")
+        "strings from database fields handled by vinaigrette."
+    )
 
     def add_arguments(self, parser):
         super(Command, self).add_arguments(parser)
-        parser.add_argument('--no-vinaigrette', default=True, action='store_false', dest='avec-vinaigrette',
-            help="Don't include strings from database fields handled by vinaigrette."),
-        parser.add_argument('--keep-obsolete', default=False, action='store_true', dest='keep-obsolete',
-            help="Don't obsolete strings no longer referenced in code or Viniagrette's fields.")
-        parser.add_argument('--keep-vinaigrette-temp', default=False, action='store_true', dest='keep-vinaigrette-temp',
-            help="Keep the temporary vinaigrette-deleteme.py file.")
+        parser.add_argument(
+            '--no-vinaigrette', default=True, action='store_false', dest='avec-vinaigrette',
+            help="Don't include strings from database fields handled by vinaigrette."
+        )
+        parser.add_argument(
+            '--keep-obsolete', default=False, action='store_true', dest='keep-obsolete',
+            help="Don't obsolete strings no longer referenced in code or Viniagrette's fields."
+        )
+        parser.add_argument(
+            '--keep-vinaigrette-temp', default=False, action='store_true', dest='keep-vinaigrette-temp',
+            help="Keep the temporary vinaigrette-deleteme.py file."
+        )
 
     requires_system_checks = True
 
@@ -129,6 +140,7 @@ class Command(django_makemessages.Command):
                 os.unlink(vinfilepath)
 
         r_lineref = re.compile(r'%s:(\d+)' % re.escape(vinfilepath))
+
         def lineref_replace(match):
             try:
                 return sources[int(match.group(1))]
@@ -142,52 +154,43 @@ class Command(django_makemessages.Command):
             po_paths = _get_po_paths()
         else:
             locales = options.get('locale')
-
-            # In django 1.6+ one or more locales can be specified, so we
-            # make sure to handle both versions here.
-
-            # Also, there is no simple way to differentiate a string from a
-            # sequence of strings that works in both python2 (for str and
-            # unicode) and python3 so we query for a string method on locales.
-            if hasattr(locales, 'capitalize'):
-                locales = [locales]
-
             po_paths = _get_po_paths(locales)
 
         if options.get('keep-obsolete'):
-            obsolete_warning = ['#. %s\n' %
-                ugettext('Obsolete translation kept alive with Viniagrette').encode('utf8'),
-                '#: obsolete:0\n']
+            obsolete_warning = [
+                '#. {}\n'.format(
+                    gettext('Obsolete translation kept alive with Viniagrette')
+                ),
+                '#: obsolete:0\n',
+            ]
 
         for po_path in po_paths:
-            po_file = open(po_path)
-            new_contents = []
-            lastline = ''
-            for line in po_file:
-                if line.startswith('#: '):
-                    new_contents.append(r_lineref.sub(lineref_replace, line))
-                else:
-                    if (line.startswith('#, python-format')
-                      and lastline.startswith('#: ')
-                      and vinfilepath in lastline):
-                        # A database string got labelled as being python-format;
-                        # it shouldn't be. Skip the line.
-                        continue
-                    if options.get('keep-obsolete'):
-                        if line in obsolete_warning:
-                            # Don't preserve old obsolete warnings we inserted
+            with open(po_path) as po_file:
+                new_contents = []
+                lastline = ''
+                for line in po_file:
+                    if line.startswith('#: '):
+                        new_contents.append(r_lineref.sub(lineref_replace, line))
+                    else:
+                        if (line.startswith('#, python-format')
+                                and lastline.startswith('#: ')
+                                and vinfilepath in lastline):
+                            # A database string got labelled as being python-format;
+                            # it shouldn't be. Skip the line.
                             continue
-                        if line.startswith('#~ msgid '):
-                            new_contents.extend(obsolete_warning)
-                        if line.startswith('#~ '):
-                            line = re.sub(r'^#~ ', '', line)
+                        if options.get('keep-obsolete'):
+                            if line in obsolete_warning:
+                                # Don't preserve old obsolete warnings we inserted
+                                continue
+                            if line.startswith('#~ msgid '):
+                                new_contents.extend(obsolete_warning)
+                            if line.startswith('#~ '):
+                                line = re.sub(r'^#~ ', '', line)
 
-                    new_contents.append(line)
-                lastline = line
-            po_file.close()
+                        new_contents.append(line)
+                    lastline = line
 
             # Perhaps this should be done a little more atomically w/ renames?
-            po_file = open(po_path, 'w')
-            for line in new_contents:
-                po_file.write(line)
-            po_file.close()
+            with open(po_path, 'w') as po_file:
+                for line in new_contents:
+                    po_file.write(line)
